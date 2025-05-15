d7ac4183ef4b764abd9ad4d5adc9c2f4ee70630c
Name <you@example.com>
2025-05-15 23:31:11 +0530

    rg code updated

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "4.26.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.26.0"
     }
-    backend "azurerm" {
-        resource_group_name  = "rajan"
-        storage_account_name = "pipelinestorageaccoun4"
-        container_name       = "pipelinecontainer"
-        key                 = "github.tfstate"
-      
-    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rajan"
+    storage_account_name = "pipelinestorageaccoun4"
+    container_name       = "pipelinecontainer"
+    key                  = "github.tfstate"
+
+  }
 }
 provider "azurerm" {
-    features {}
+  features {}
   subscription_id = "0cead613-b99d-4057-9b73-e282208bb4c4"
 }
 resource "azurerm_resource_group" "rg" {
