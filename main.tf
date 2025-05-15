71ae73245484747e04ebe4fc01fcbc8330a27136
Name <you@example.com>
2025-05-15 23:15:10 +0530

    rg code added

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "4.26.0"
+        }
+    }
+    backend "azurerm" {
+        resource_group_name  = "rajan"
+        storage_account_name = "pipelinestorageaccoun4"
+        container_name       = "pipelinecontainer"
+        key                 = "github.tfstate"
+      
+    }
+}
+provider "azurerm" {
+    features {}
+  subscription_id = "0cead613-b99d-4057-9b73-e282208bb4c4"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "github-action-rg"
+  location = "East US"
+}
